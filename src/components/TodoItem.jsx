0d4a4c8408719ffc2ae0b13e131f11b8bc98bdf0
Nymisha <kmsnymisha@gmail.com>
2021-11-15 20:27:40 +0530

    Add existing file

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div className="add-todo-container">Todo List Component</div>
+    );
+}
+
+export default TodoList;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddTodo = () => {
+    return (
+        <div className="add-todo-container">Add Todo Component</div>
+    );
+}
+
+export default AddTodo;
