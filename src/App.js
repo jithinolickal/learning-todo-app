494eb193fdc4ccc16386ba76c2fae03d791d9c1b
Nymisha-Komuravelli <42866863+Nymisha-Komuravelli@users.noreply.github.com>
2021-11-17 19:34:50 +0530

    Created state objects for todo items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import './App.css';
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import { Row, Col } from 'antd';
 
-function App() { 
+function App() {
+
+  const [todo, setTodo] = useState();
+
+  const handleTodo = (value) => {
+    setTodo(value);
+  }
+
   return (
     <div className="App">
+      <div style={{color: "white"}}>{JSON.stringify(todo)}</div>
       <div className="app-title">TO DO</div>
-        <Row align="middle" className="page-center">
-            <Col span={12} offset={6}>
-              <AddTodo/>
-              <TodoList/>
-            </Col>
-        </Row>
+      <Row align="middle" className="page-center">
+        <Col span={12} offset={6}>
+          <AddTodo handleTodo={handleTodo}/>
+          <TodoList newTodo={todo}/>
+        </Col>
+      </Row>
     </div>
   );
 }
