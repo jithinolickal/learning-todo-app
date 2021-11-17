2054031ae0d0f8cdf845febbd39893105ab16c9f
Nymisha-Komuravelli <42866863+Nymisha-Komuravelli@users.noreply.github.com>
2021-11-17 20:42:43 +0530

    Code changes for add todo item
    Code changes for add new todo item and displaying it

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = (props) => {
-
-    const [todoList, setTodoList] = useState([]);
-
-    useEffect(() => {
-        setTodoList([
-            ...todoList,
-            props.newTodo
-        ]);
-    }, [props.newTodo])
-
-    return (
-        <>
-            {/* <div style={{ color: "white" }}>{JSON.stringify(todoList)}</div> */}
-            {
-                todoList.map((todo, index) => <TodoItem key={index} todo={todo}/>)
-            }
-        </>
-    );
-}
+  return (
+    <>
+      {props.todoList?.map((todo, index) => (
+        <TodoItem key={index} todo={todo} />
+      ))}
+    </>
+  );
+};
 
 export default TodoList;
