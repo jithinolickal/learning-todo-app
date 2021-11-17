34f4bc3f4f31ce69847e0a31cc17221d39bc6181
Nymisha-Komuravelli <42866863+Nymisha-Komuravelli@users.noreply.github.com>
2021-11-17 20:43:33 +0530

    Code changes for add new todo
    Code changes for add new todo item and displaying it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 
 function App() {
-
-  const [todo, setTodo] = useState();
+  const [todoList, setTodoList] = useState([
+    {
+      taskName: "aaa",
+      taskDesc: "aaa",
+    },
+    {
+      taskName: "bbb",
+      taskDesc: "bbb",
+    },
+  ]);
 
   const handleTodo = (value) => {
-    setTodo(value);
-  }
+    setTodoList([...todoList, value]);
+  };
 
   return (
     <div className="App">
-      <div style={{color: "white"}}>{JSON.stringify(todo)}</div>
       <div className="app-title">TO DO</div>
       <Row align="middle" className="page-center">
         <Col span={12} offset={6}>
-          <AddTodo handleTodo={handleTodo}/>
-          <TodoList newTodo={todo}/>
+          <AddTodo handleTodo={handleTodo} />
+          <TodoList todoList={todoList} />
         </Col>
       </Row>
     </div>
