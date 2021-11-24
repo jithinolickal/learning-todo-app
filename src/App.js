7b8a9499886227fec85fa054b48e7fc80fd599ac
Nymisha-Komuravelli <42866863+Nymisha-Komuravelli@users.noreply.github.com>
2021-11-24 18:17:37 +0530

    Added Delete functionality
    Added Delete functionality and implemented data transfer using Context API.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import { Row, Col } from "antd";
 
+export const TodoContext = createContext();
+
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [selectedValue, setSelectedValue] = useState();
 
   const handleTodo = (value) => {
-    let tempTodo = { ...value };
-    tempTodo.id = todoList.length + 1;
-    let newTodo = tempTodo;
-    setTodoList([...todoList, newTodo]);
+    setTodoList([
+      ...todoList,
+      { ...value, id: Date.now(), isComplete: false },
+    ]);
   };
   const handleDelete = (id) => {
     setTodoList(todoList.filter((todo) => todo.id != id));
   };
+  const handleComplete = (todo1) => {
+    let selectedindex = todoList.findIndex((todo) => todo.id == todo1.id);
+    let newTodoList = [...todoList];
+    newTodoList[selectedindex] = {
+      ...newTodoList[selectedindex],
+      isComplete: todo1.isComplete,
+    };
+    setTodoList(newTodoList);
+  };
 
   return (
     <div className="App">
+      {/* <p style={{ color: "white" }}>{JSON.stringify(todoList)}</p> */}
       <div className="app-title">TO DO</div>
       <Row align="middle" className="page-center">
         <Col span={12} offset={6}>
-          <AddTodo handleTodo={handleTodo} />
-          <TodoList todoList={todoList} handleDelete={handleDelete} />
+          <TodoContext.Provider value={[todoList, setTodoList]}>
+            <AddTodo handleTodo={handleTodo} />
+            <TodoList
+              handleDelete={handleDelete}
+              handleComplete={handleComplete}
+            />
+          </TodoContext.Provider>
         </Col>
       </Row>
     </div>
