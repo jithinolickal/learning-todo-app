fee9b9a604071c279f40e691ccb4ff944fce00f5
Nymisha-Komuravelli <42866863+Nymisha-Komuravelli@users.noreply.github.com>
2021-11-24 18:18:21 +0530

    Added Delete functionality
    Added Delete functionality and implemented data transfer using Context API.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,10 +3,16 @@ import { Col, Row, Button } from "antd";
 import { DeleteFilled } from "@ant-design/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react/cjs/react.development";
 
 const TodoItem = (props) => {
+
   const handleDelete = () => {
     props.handleDelete(props.todo.id);
+  };
+  const handleComplete = () => {
+    props.todo.isComplete = true;
+    props.handleComplete(props.todo);
   };
 
   return (
@@ -15,25 +21,26 @@ const TodoItem = (props) => {
         <Col xs={20} sm={20} md={20} lg={18} xl={18} xxl={20}>
           <Row justify="start">
             <Col>
-              <h3 className="item-title">{props.todo?.taskName}</h3>
+              <h3 className={props.todo.isComplete ? "complete-todo-title item-title": "item-title"}>{props.todo?.taskName}</h3>
             </Col>
           </Row>
           <Row justify="start">
             <Col>
-              <span className="item-desc">{props.todo?.taskDesc}</span>
+              <span className={props.todo.isComplete ? "complete-todo-desc item-desc": "item-desc"}>{props.todo?.taskDesc}</span>
             </Col>
           </Row>
         </Col>
         <Col xs={4} sm={4} md={4} lg={6} xl={6} xxl={4}>
           <Row gutter={[8, 8]} justify="end">
             <Col>
-              <Button className="icon-red">
+              <Button className="icon-red" onClick={handleComplete} hidden={props.todo.isComplete}
+              >
                 <FontAwesomeIcon icon={faCheckDouble} />
               </Button>
             </Col>
             <Col>
-              <Button className="icon-red">
-                <DeleteFilled onClick={handleDelete} />
+              <Button className="icon-red"  onClick={handleDelete} >
+                <DeleteFilled/>
               </Button>
             </Col>
           </Row>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,48 @@
-import React, { useEffect, useState } from "react";
+import { TodoContext } from "App";
+import React, { useContext, useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = (props) => {
   const handleDelete = (id) => {
     props.handleDelete(id);
   };
+  const handleComplete = (todo) => {
+    props.handleComplete(todo);
+  };
+  /* const [a, setA] = useContext(TodoContext);
+  useEffect(() => {
+    setA([...a, { taskName: "Task2", taskDesc: "Desc2", id: 3 }]);
+  }, []); */
 
   return (
     <>
-      {props.todoList?.map((todo, key) => {
-        console.log(todo.id);
-        return (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleDelete={() => handleDelete(todo.id)}
-          />
-        );
-      })}
+      <TodoContext.Consumer>
+        {(TodoList) => (
+          <>
+            {TodoList[0].map((todo, key) => {
+              return (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  handleDelete={() => handleDelete(todo.id)}
+                  handleComplete={() => handleComplete(todo)}
+                />
+              );
+            })}
+          </>
+        )}
+      </TodoContext.Consumer>
+      {/* {a.map((todo, key) => {
+              console.log(todo);
+              return (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  handleDelete={() => handleDelete(todo.id)}
+                />
+              );
+            })
+          } */}
     </>
   );
 };
